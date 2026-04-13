709418a444c27020ca105e431dc83885edce4064
Shane <theshanukg@gmail.com>
2026-04-12 23:30:45 -0600

    part2

--- main.py
+++ main.py
@@ -13,10 +13,25 @@ def is_safe(levels):
             return False
     return True
 
-count = 0
+def is_safe_dampened(levels):
+    if is_safe(levels):
+        return True
+    for i in range(len(levels)):
+        if is_safe(levels[:i] + levels[i + 1:]):
+            return True
+    return False
+
+count_p1 = 0
+count_p2 = 0
 for line in open(sys.argv[1] if len(sys.argv) > 1 else "input.txt"):
     levels = list(map(int, line.split()))
-    if levels and is_safe(levels):
-        count += 1
+    if not levels:
+        continue
+    if is_safe(levels):
+        count_p1 += 1
+        count_p2 += 1
+    elif is_safe_dampened(levels):
+        count_p2 += 1
 
-print(count)
+print(f"Part 1: {count_p1}")
+print(f"Part 2: {count_p2}")
